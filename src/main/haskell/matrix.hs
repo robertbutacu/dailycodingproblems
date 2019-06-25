5d6612f35b7364e1b1146819c7b5fd3ddc7f74fb
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-06-25 16:45:57 +0100

    printing test wtv

--- src/main/haskell/matrix.hs
+++ src/main/haskell/matrix.hs
@@ -24,4 +24,4 @@ snakeRepresentation []     = []
 snakeRepresentation [h]    = h
 snakeRepresentation matrix = (obtainOuterLayer matrix) ++ (snakeRepresentation $ recurseIntoMatrix $ matrix)
 
-main = do print "123"
+main = do print $ snakeRepresentation $ [[1,2,3,4,5], [6,7,8, 9, 10], [11, 12, 13, 14, 15], [16,17,18,19, 20]]
